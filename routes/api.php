e2f02d2ad526f193e46e162d40be3400e9761db1
Fesposito9197 <kekkoesp8@gmail.com>
2023-02-07 15:35:30 +0100

    aggiunge invio e controlli email

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ContactFormController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
+Route::post('contact-form', [ContactFormController::class, 'email']);
